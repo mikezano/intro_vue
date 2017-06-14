671133f6f668bd9523e6349ecb274b76e48a1834
Michael Manzano <michael.a.manzano@intel.com>
2017-06-14 13:40:53 -0700

    bounce

--- src/components/dialog.vue
+++ src/components/dialog.vue
@@ -2,9 +2,10 @@
 	<div class="main">
 		<h3>Let's trigger this here modal!</h3>
 			<button @click="toggleShow">
-			<span v-if="isShowing">Hide child</span>
-			<span v-else>Show child</span>
-		</button>
+				<span v-if="isShowing">Hide child</span>
+				<span v-else>Show child</span>
+			</button>
+			<button @click="toggleBounce">Bounce</button>
 		<transition name="fade">
 			<modal v-if="isShowing" class="modal">
 				<button @click="toggleShow">
@@ -12,11 +13,22 @@
 				</button>
 			</modal>
 		</transition>
+		<transition
+			name="ballmove"
+			enter-active-class="bouncein"
+		>
+			<ball v-if="isBouncing" class="ball"></ball>
+		</transition>
+
+		<!--Templates-->
 		<script type="text/x-template" id="childarea">
 			<div>
 				<h2>Here I am!</h2>
 				<slot></slot>
 			</div>
+		</script>
+		<script type="text/x-template" id="ball">
+			<div>Ball</div>
 		</script>
 	</div>	
 </template>
@@ -29,25 +41,34 @@ const Child = {
 	template: '#childarea'
 };
 
+const Ball = {
+	template: '#ball'
+}
+
 export default {
   	data() {
     	return {
-      		isShowing: false
+      		isShowing: false,
+			isBouncing: false
     	}
   	},
   	methods: {
     	toggleShow() {
       		this.isShowing = !this.isShowing;
-    	}
+    	},
+		toggleBounce() {
+			this.isBouncing = !this.isBouncing;
+		}
   	},
   	components: {
-   		modal: Child
+   		modal: Child,
+		ball: Ball
  	}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 
 .main{
 	width:100%;
@@ -65,4 +86,28 @@ export default {
 .fade-enter, .fade-leave-to {
   opacity: 0;
 }
+
+@mixin ballb($yaxis: 0) {
+  transform: translate3d(0, $yaxis, 0);
+}
+
+@keyframes bouncein { 
+  1% { @include ballb(-400px); }
+  20%, 40%, 60%, 80%, 95%, 99%, 100% { @include ballb() }
+  30% { @include ballb(-80px); }
+  50% { @include ballb(-40px); }
+  70% { @include ballb(-30px); }
+  90% { @include ballb(-15px); }
+  97% { @include ballb(-10px); }
+}
+
+.bouncein { 
+  animation: bouncein 0.8s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+}
+
+.ballmove-enter {
+  @include ballb(-400px);
+}
+
+//.ball{background-color:red;}
 </style>
